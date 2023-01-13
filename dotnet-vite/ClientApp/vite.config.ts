48d5be9fbd02f1b88cde0acdb53692ed1089f80a
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2023-01-12 21:09:52 -0600

    Published upgraded package. Using package in React and Svelte samples.

--- dotnet-vite/ClientApp/vite.config.ts
+++ dotnet-vite/ClientApp/vite.config.ts
@@ -2,72 +2,8 @@ import { defineConfig } from 'vite'
 import type { UserConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { basename, posix } from 'path'
+import { ViteDotNetPlugin } from 'vite-dotnet';
 
-type PluginConfig = {
-  port: number;
-  appFolder: string;
-  entrypoint: string;
-  prodServerOrigin?: string; //Not for initial release. Use when hosting app files in a remote server such as S3 or Azure Blob.
-}
-
-const defaultPort = 5173;
-const defaultAppFolder = "ClientApp";
-
-function outputOptions (assetsDir: string) {
-  // Internal: Avoid nesting entrypoints unnecessarily.
-  const outputFileName = (ext: string) => ({ name }: { name: string }) => {
-    const shortName = basename(name).split('.')[0]
-    return posix.join(assetsDir, `${shortName}.[hash].${ext}`)
-  }
-
-  return {
-    entryFileNames: outputFileName('js'),
-    chunkFileNames: outputFileName('js'),
-    assetFileNames: outputFileName('[ext]'),
-  }
-}
-
-function ViteDotNetPlugin(entrypoint: string) {
-
-  return ViteDotNet({ port: defaultPort, appFolder: defaultAppFolder, entrypoint: entrypoint });
-}
-
-function ViteDotNet(config: PluginConfig) {
-  return {
-    name: 'ViteDotNet',
-    enforce: "post" as const,
-    config: (userConfig: UserConfig, { command, mode }) => {
-
-      //https://vitejs.dev/config/server-options.html#server-origin
-
-      return {
-        server: {
-          strictPort: true,
-          origin: `http://localhost:${config.port}`,
-          /*proxy:{
-            '*' : {
-              target: 'https://localhost:7167',
-              changeOrigin: true
-            }
-          },*/
-          hmr: {
-            protocol: 'ws'
-          }
-        },
-        build: {
-          outDir: `../wwwroot`,
-          emptyOutDir: false,
-          manifest: `${config.appFolder}/manifest.json`,
-          rollupOptions: {
-            // overwrite default .html entry
-            input: config.entrypoint,
-            output: outputOptions(config.appFolder)
-          }
-        }
-      }
-    }
-  };
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
--- dotnet-vite/ReactApp/vite.config.ts
+++ dotnet-vite/ReactApp/vite.config.ts
@@ -1,59 +1,15 @@
 import { defineConfig } from 'vite'
-import type { UserConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import type { PluginConfig } from 'vite-dotnet'
+import ViteDotNet from 'vite-dotnet'
 
-type PluginConfig = {
-  port: number;
-  appFolder: string;
-  entrypoint: string;
-  prodServerOrigin?: string; //Not for initial release. Use when hosting app files in a remote server such as S3 or Azure Blob.
+const config: PluginConfig = {
+  port: 5174,
+  appFolder: "ReactApp",
+  entrypoint: "src/main.tsx"
 }
-
-const defaultPort = 5174;
-const defaultAppFolder = "ReactApp";
-
-
-function ViteDotNetPlugin(entrypoint: string) {
-
-  return ViteDotNet({ port: defaultPort, appFolder: defaultAppFolder, entrypoint: entrypoint });
-}
-
-function ViteDotNet(config: PluginConfig) {
-  return {
-    name: 'ViteDotNet',
-    enforce: "post" as const,
-    config: (userConfig: UserConfig, { command, mode }) => {
-
-      //https://vitejs.dev/config/server-options.html#server-origin
-
-      return {
-        server: {
-          origin: `http://localhost:${config.port}`,
-          /*proxy:{
-            '*' : {
-              target: 'https://localhost:7167',
-              changeOrigin: true
-            }
-          },*/
-          hmr: {
-            protocol: 'ws'
-          }
-        },
-        build: {
-          outDir: `../wwwroot/${config.appFolder}`,
-          emptyOutDir: true,
-          manifest: true,
-          rollupOptions: {
-            // overwrite default .html entry
-            input: config.entrypoint
-          }
-        }
-      }
-    }
-  };
-};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteDotNetPlugin("src/main.tsx")],
+  plugins: [react(), ViteDotNet(config)],
 })
