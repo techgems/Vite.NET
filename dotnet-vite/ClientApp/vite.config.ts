db024d258eadf1be4a4836a7335f776d126fe230
Carlos Jose Jimenez <cjimenez@deckprismsports.com>
2023-02-01 11:04:24 -0600

    Fix issues with npm package.

--- dotnet-vite/ClientApp/vite.config.ts
+++ dotnet-vite/ClientApp/vite.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 import ViteDotNet from 'vite-dotnet';
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte(), ViteDotNet("src/main.ts")]
--- dotnet-vite/ReactApp/vite.config.ts
+++ dotnet-vite/ReactApp/vite.config.ts
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import type { PluginConfig } from 'vite-dotnet'
 import ViteDotNet from 'vite-dotnet'
-
-const config: PluginConfig = {
-  port: 5174,
-  appFolder: "ReactApp",
-  entrypoint: "src/main.tsx"
-}
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteDotNet(config)],
+  plugins: [react(), ViteDotNet("src/main.tsx", 5174, "ReactApp")],
 })
